083a51b9d1e4de8a66664e3bb1da2e83531edd4b
hrhr_313350 <tkaruha.205@gmail.com>
2026-09-11 02:34:26 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -6417,7 +6417,7 @@ function render(data) {
         <td>${stage.element}</td>
         <td>${Array.isArray(stage.piece) ? stage.piece.join(', ') : stage.piece}</td>
         <td>${Array.isArray(stage.gimmick) ? stage.gimmick.join(', ') : (stage.gimmick || '')}</td>
-        <td>${stage.mission || ''}</td>
+        <td>${Array.isArray(stage.missiontype) ? stage.missiontype.join(', ') : stage.missiontype}</td>
         <td>${Array.isArray(stage.drop) ? stage.drop.join(', ') : stage.drop}</td>
         </tr>
     `).join("");
